a7752399a50c02b47a624e89cf02971bf4528957
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-21 12:09:40 +0200

    Download All Fonts

--- scripts/swiper-handler.js
+++ scripts/swiper-handler.js
@@ -1,44 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize the first Swiper instance for .swiper-container2
     var swiper2 = new Swiper('.swiper-container2', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
         slidesPerView: 'auto',
         spaceBetween: 20,
+        autoplay: {
+            delay: 3000, // مدة التمرير التلقائي بالمللي ثانية (3 ثواني)
+            disableOnInteraction: false, // استمر في التمرير التلقائي حتى بعد التفاعل
+        },
     });
 
     // Initialize the main Swiper instance for .swiper-container
     var swiper = new Swiper('.swiper-container', {
         loop: false,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
         slidesPerView: 'auto',
         spaceBetween: 20,
+        autoplay: {
+            delay: 3000, // مدة التمرير التلقائي بالمللي ثانية (3 ثواني)
+            disableOnInteraction: false, // استمر في التمرير التلقائي حتى بعد التفاعل
+        },
         on: {
             slideChange: function () {
                 // Ensure the swiper instance is fully initialized
                 if (!swiper || !swiper.slides) {
                     return;
                 }
-
-                // Remove transparency from all slides
-                document.querySelectorAll('.swiper-slide').forEach(slide => {
-                    slide.style.opacity = '1';
-                });
-
-                // Apply transparency to partially visible slides
-                swiper.slides.forEach(slide => {
-                    const rect = slide.getBoundingClientRect();
-                    const isPartiallyVisible = (rect.left < window.innerWidth && rect.right > window.innerWidth) || (rect.right > 0 && rect.left < 0);
-
-                    if (isPartiallyVisible) {
-                        slide.style.opacity = '0.5';
-                    }
-                });
             },
             init: function () {
                 this.emit('slideChange');
